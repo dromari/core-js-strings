23838eb3e46ae5f679acd1b5893d7c7ef8afc333
Dromari <7709722@gmail.com>
2025-09-28 00:36:59 +0300

    feat: last of solution for tasck

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -184,13 +184,15 @@ function removeLastOccurrences(str, value) {
  *   sumOfCodes('') => 0
  *   sumOfCodes() => 0
  */
-function sumOfCodes(/* str */) {
-  throw new Error('Not implemented');
-  // let sum = 0;
-  // for (let i = 0; i < str.length; i += 1) {
-  //   sum += str.charCodeAt(i);
-  // }
-  // return sum;
+function sumOfCodes(str) {
+  if (!str) {
+    return 0;
+  }
+  let sum = 0;
+  for (let i = 0; i < str.length; i += 1) {
+    sum += str.charCodeAt(i);
+  }
+  return sum;
 }
 
 /**
